f883df2c530fc64cbf5ff0e7f75bc8a41d0a06e3
Jonathan Miller <jonathanmiller2@hotmail.com>
2021-03-06 05:21:39 +0000

    Cron running a python script inside a docker container inserting values into a database in another docker container every minute

--- data-retrieval/app/scripts/pop_db_test.py
+++ data-retrieval/app/scripts/pop_db_test.py
@@ -1,17 +1,30 @@
 import os
 import psycopg2
+
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
 from datetime import datetime
 from random import random
 
-SQL_HOST = os.getenv('SQL_HOST')
-SQL_DATABASE = os.getenv('SQL_DATABASE')
-SQL_USER = os.getenv('SQL_USER')
-SQL_PASSWORD = os.getenv('SQL_PASSWORD')
-SQL_PORT = os.getenv('SQL_PORT')
+sqlhost = os.environ.get('SQL_HOST')
+sqldb = os.environ.get('SQL_DATABASE')
+sqlusr = os.environ.get('SQL_USER')
+sqlpass = os.environ.get('SQL_PASSWORD')
+sqlport = os.environ.get('SQL_PORT')
 
-conn = psycopg2.connect(host=SQL_HOST, database=SQL_DATABASE, user=SQL_USER, password=SQL_PASSWORD, port=SQL_PORT)
+#print('Connecting to DB with following parameters:')
+#print('HOST=' + sqlhost)
+#print('SQL_DB=' + sqldb)
+#print('USER=' + sqlusr)
+#print('PW=' + sqlpass)
+#print('PORT=' + sqlport)
+
+conn = psycopg2.connect(host=sqlhost, database=sqldb, user=sqlusr, password=sqlpass, port=sqlport)
 cur = conn.cursor()
-cur.execute("INSERT INTO randseries(timestamp, val) VALUES (%s, %s)", (datetime.now(), random()*100)))
+cur.execute('INSERT INTO randseries(timestamp, val) VALUES (%s, %s)', (datetime.now(), random()*100))
+
+print("Inserted new value")
 
 conn.commit()
 cur.close()
