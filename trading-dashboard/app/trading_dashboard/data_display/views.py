9c28cb0858641fec6e7c6b7810407ed083bf99fb
Jonathan Miller <jonathanmiller2@hotmail.com>
2021-03-12 10:08:22 +0000

    Fixed timestamp issues. Added period select buttons on dashboard

--- trading-dashboard/app/trading_dashboard/data_display/views.py
+++ trading-dashboard/app/trading_dashboard/data_display/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
+from datetime import datetime, timedelta
 import requests, json
 
 from trading_dashboard.data_display.models import Randseries
@@ -9,7 +10,12 @@ from trading_dashboard.data_display.models import Randseries
 def index(request):
     return render(request, 'index.html')
 
-def getval(request):
+def get_newest_val(request):
     data = serializers.serialize('json', Randseries.objects.order_by('-timestamp')[:1])
+    return HttpResponse(data)
 
+def get_historical_vals(request):
+    period = int(request.GET['period'])
+    query = Randseries.objects.filter(timestamp__lte=datetime.now(), timestamp__gt=datetime.now()-timedelta(hours=period)).order_by('timestamp')
+    data = serializers.serialize('json', query)
     return HttpResponse(data)
