e04d6247b9c9558991323ea3c8369c840bfc41b0
Jonathan Miller <jonathanmiller2@hotmail.com>
2021-03-29 05:49:05 +0000

    Values are now being requested from alphavantage

--- data-retrieval/app/scripts/pop_db_test.py
+++ data-retrieval/app/scripts/pop_db_test.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+import requests
 
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
@@ -12,6 +13,7 @@ sqldb = os.environ.get('SQL_DATABASE')
 sqlusr = os.environ.get('SQL_USER')
 sqlpass = os.environ.get('SQL_PASSWORD')
 sqlport = os.environ.get('SQL_PORT')
+avkey = os.environ.get('ALPHA_VANTAGE_API_KEY')
 
 #print('Connecting to DB with following parameters:')
 #print('HOST=' + sqlhost)
@@ -22,7 +24,13 @@ sqlport = os.environ.get('SQL_PORT')
 
 conn = psycopg2.connect(host=sqlhost, database=sqldb, user=sqlusr, password=sqlpass, port=sqlport)
 cur = conn.cursor()
-cur.execute('INSERT INTO randseries(timestamp, val) VALUES (%s, %s)', (datetime.now(), random()*100))
+
+symbol = "GME"
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=1min&outputsize=compact&apikey={avkey}"
+response = requests.get(url).json()
+value = list(response['Time Series (1min)'].values())[0]['4. close']
+
+cur.execute(f'INSERT INTO ticker_{symbol}(timestamp, val) VALUES (%s, %s)', (datetime.now(), value))
 
 print("Inserted new value")
 
--- trading-dashboard/app/trading_dashboard/data_display/views.py
+++ trading-dashboard/app/trading_dashboard/data_display/views.py
@@ -3,19 +3,24 @@ from django.http import HttpResponse
 from django.core import serializers
 from datetime import datetime, timedelta
 import requests, json
-
-from trading_dashboard.data_display.models import Randseries
+from django.apps import apps
 
 
 def index(request):
     return render(request, 'index.html')
 
 def get_newest_val(request):
-    data = serializers.serialize('json', Randseries.objects.order_by('-timestamp')[:1])
+    ticker = request.GET['ticker']
+    
+    ticker_model = apps.get_model(app_label='data_display', model_name='Ticker_'+ticker)
+    data = serializers.serialize('json', ticker_model.objects.order_by('-timestamp')[:1])
     return HttpResponse(data)
 
 def get_historical_vals(request):
     period = int(request.GET['period'])
-    query = Randseries.objects.filter(timestamp__lte=datetime.now(), timestamp__gt=datetime.now()-timedelta(hours=period)).order_by('timestamp')
+    ticker = request.GET['ticker']
+
+    ticker_model = apps.get_model(app_label='data_display', model_name='Ticker_'+ticker)
+    query = ticker_model.objects.filter(timestamp__lte=datetime.now(), timestamp__gt=datetime.now()-timedelta(hours=period)).order_by('timestamp')
     data = serializers.serialize('json', query)
     return HttpResponse(data)
